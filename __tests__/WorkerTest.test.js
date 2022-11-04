3539d2ea40c5b119f481d4106690c1cd8964276d
bagustri3 <tribagus0510@gmail.com>
2022-11-04 11:27:01 +0700

    workers test

--- __tests__/WorkerTest.test.js
+++ __tests__/WorkerTest.test.js
@@ -4,3 +4,73 @@ const { Worker, sequelize } = require("../models");
 const bcrypt = require("bcryptjs");
 const { queryInterface } = sequelize;
 
+describe.skip("POST /workers/register", () => {
+  afterAll(() => {
+    sequelize.queryInterface.bulkDelete("Workers", null, {
+      truncate: true,
+      restartIdentity: true,
+      cascade: true,
+    });
+  });
+
+  afterEach(() => {
+    sequelize.queryInterface.bulkDelete("Workers", null, {
+      truncate: true,
+      restartIdentity: true,
+      cascade: true,
+    });
+  });
+
+  describe("POST /workers/register - Email and Password Pass", () => {
+    it("should respond with status code 201 and returning id and email", async () => {
+      const payloadRegisterSuccess = {
+        username: "workers test",
+        email: "workertest@gmail.com",
+        password: "12345678",
+        phoneNumber: "0812345678",
+        address: "Bandung",
+      };
+      const result = await request(app)
+        .post("/workers/register")
+        .send(payloadRegisterSuccess);
+      expect(result.status).toBe(201);
+      expect(result.body).toBeInstanceOf(Object);
+      expect(result.body).toHaveProperty("id", expect.any(Number));
+      expect(result.body).toHaveProperty("workers", expect.any(String));
+    });
+  });
+
+  describe("POST /workers/register - Email key is missing", () => {
+    it("should respond with status code 400 and returning error message", async () => {
+      const payloadWorkersMiss = {
+        username: "workers test",
+        // email: "workertest@gmail.com",
+        password: "12345678",
+        phoneNumber: "0812345678",
+        address: "Bandung",
+      };
+      const result = await request(app)
+        .post("/workers/register")
+        .send(payloadWorkersMiss);
+      expect(result.status).toBe(400);
+      expect(result.body).toHaveProperty("message", "Email is required");
+    });
+  });
+
+  describe("POST /workers/register - Email key is null", () => {
+    it("should respond with status code 400 and returning error message", async () => {
+      const payloadWorkersMiss = {
+        username: "workers test",
+        email: "",
+        password: "12345678",
+        phoneNumber: "0812345678",
+        address: "Bandung",
+      };
+      const result = await request(app)
+        .post("/workers/register")
+        .send(payloadWorkersMiss);
+      expect(result.status).toBe(400);
+      expect(result.body).toHaveProperty("message", "Email is required");
+    });
+  });
+});
