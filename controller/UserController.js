46ca1d771f940cf703057cff5f3e1abdcb0c13e7
bagustri3 <tribagus0510@gmail.com>
2022-11-05 01:50:53 +0700

    login, edit, find one

--- controller/UserController.js
+++ controller/UserController.js
@@ -14,46 +14,51 @@ class UserController {
         address,
         DeviceId,
       });
-      console.log(result)
-      res.status(201).json({message : "Created new User"})
+      console.log(result);
+      res.status(201).json({ message: "Created new User" });
     } catch (error) {
       next(error);
     }
   }
   static async login(req, res, next) {
     try {
-      const { email, password } = req.body
+      const { email, password } = req.body;
       const user = await User.findOne({
-        where : {
-          email
-        }
-      })
-      if(!user) throw { name : "Invalid email/password" }
-      const validatePassword = compare(password, user.password)
-      if(!validatePassword) throw { name: "Invalid email/password" }
+        where: {
+          email,
+        },
+      });
+      if (!user) throw { name: "Invalid email/password" };
+      const validatePassword = compare(password, user.password);
+      if (!validatePassword) throw { name: "Invalid email/password" };
       const payload = {
-        id : user.id,
-        email: user.email
-      }
-      const access_token = sign(payload)
-      res.status(200).json({access_token: access_token, id: user.id})
+        id: user.id,
+        email: user.email,
+      };
+      const access_token = sign(payload);
+      res.status(200).json({ access_token: access_token, id: user.id });
     } catch (error) {
       next(error);
     }
   }
   static async profile(req, res, next) {
     try {
-      const { id } = req.params
-      const result = await User.findByPk(id)
-      res.status(200).json(result)
+      const { id } = req.user;
+      const result = await User.findByPk(id);
+      res.status(200).json(result);
     } catch (error) {
       next(error);
     }
   }
   static async editProfile(req, res, next) {
     try {
-      const { id } = req.params
-
+      const { id } = req.user;
+      const { fullName, address, phoneNumber } = req.body;
+      const update = await User.update(
+        { fullName, address, phoneNumber },
+        { where: { id } }
+      );
+      res.status(200).json({message : "Profile updated"});
     } catch (error) {
       next(error);
     }
