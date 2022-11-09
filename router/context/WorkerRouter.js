42b457c29bac7307ce56371f1a8d7f63b4a225b0
bagustri3 <tribagus0510@gmail.com>
2022-11-09 11:43:33 +0700

    add fetch log transaction

--- router/context/WorkerRouter.js
+++ router/context/WorkerRouter.js
@@ -1,3 +1,4 @@
+const PaymentController = require("../../controller/PaymentController");
 const ProjectController = require("../../controller/ProjectController");
 const WorkerController = require("../../controller/WorkerController");
 const { authenticationWorker } = require("../../middlewares/authentication");
@@ -7,9 +8,10 @@ const WorkerRouter = require("express").Router();
 WorkerRouter.post("/login", WorkerController.login);
 WorkerRouter.post("/register", WorkerController.register);
 WorkerRouter.use(authenticationWorker);
-WorkerRouter.get("/projects", ProjectController.fetchAllProjectWorker)
-WorkerRouter.get("/projects/:id", ProjectController.getOne)
-WorkerRouter.get("/appliedProject", ProjectController.fetchProjectWorker)
+WorkerRouter.get("/transaction", PaymentController.fetchAllTransaction);
+WorkerRouter.get("/projects", ProjectController.fetchAllProjectWorker);
+WorkerRouter.get("/projects/:id", ProjectController.getOne);
+WorkerRouter.get("/appliedProject", ProjectController.fetchProjectWorker);
 WorkerRouter.post("/:projectId", WorkerController.applyProject);
 WorkerRouter.get("/", WorkerController.profile);
 WorkerRouter.put("/:id", WorkerController.editProfile);
